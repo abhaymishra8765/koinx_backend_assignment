2ae8ea4d65fe8b6801d437398cf0bf45f95fb730
abhaymishra8765 <202111001@diu.iiitvadodara.ac.in>
2025-01-10 15:08:15 +0530

    feat: setup MongoDB connection and API routing

---
+++ app.js
@@ -0,0 +1,19 @@
+require('dotenv').config();
+const express = require('express');
+const mongoose = require('mongoose');
+const cryptoRoutes = require('./routes/cryptoRoutes');
+
+const app = express();
+app.use(express.json());
+
+mongoose.connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+})
+    .then(() => console.log('MongoDB Connected'))
+    .catch(err => console.error('MongoDB Connection Error:', err));
+
+app.use('/api', cryptoRoutes);
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
