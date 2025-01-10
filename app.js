5e66812aa4a26bafc54514febde55b021365884e
abhaymishra8765 <202111001@diu.iiitvadodara.ac.in>
2025-01-10 15:29:39 +0530

    fix: remove duplicate code in app.js

--- app.js
+++ app.js
@@ -1,4 +1,7 @@
 require('dotenv').config();
+const cron = require('node-cron');
+const fetchCryptoData = require('./utils/fetchCryptoData');
+const Crypto = require('./models/crypto'); 
 const express = require('express');
 const mongoose = require('mongoose');
 const cryptoRoutes = require('./routes/cryptoRoutes');
@@ -14,6 +17,21 @@ mongoose.connect(process.env.MONGO_URI, {
     .catch(err => console.error('MongoDB Connection Error:', err));
 
 app.use('/api', cryptoRoutes);
+
+// Schedule the job every 2 hours
+cron.schedule('0 */2 * * *', async () => {
+    console.log('Fetching cryptocurrency data...');
+    const coins = ['bitcoin', 'matic-network', 'ethereum'];
+    for (const coin of coins) {
+        const data = await fetchCryptoData(coin);
+        if (data) {
+            const crypto = new Crypto({ coin, ...data });
+            await crypto.save();
+            console.log(`${coin} data saved to database.`);
+        }
+    }
+});
+
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
